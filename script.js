e699f7c57535a2321472665e99fb1836e6341d43
yhaw12 <yooxing11@gmail.com>
2023-03-05 23:12:19 +0000

    annother update

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 let chapterHeading = document.querySelectorAll('.head');
+let passageTittle = document.querySelectorAll('.title')
+let passageHead = document.querySelectorAll('.txt')
+let psgCnt = document.querySelector('.container');
+// let passageContainers = document.querySelectorAll('.container');
 
+// main window left tab
 
-
-chapterHeading.forEach(e => {
-    e.target;
-    e.addEventListener('click', ()=>{
-        chapterHeading.forEach(item => {
-            if (e!= item){
-                item.style.display = 'blue'
-            }      
+let sideButtons =()=>{
+    chapterHeading.forEach(e => {
+        e.addEventListener('click', ()=>{
+            chapterHeading.forEach(item => {
+                if (item!= e){
+                    item.style.backgroundColor = 'initial'
+                }      
+                else{
+                    item.style.backgroundColor = 'rgb(65, 102, 158)';
+                }
+            });
+                       
         });
-            
+        e.style.transition = "all 400ms ease"
     });
-});
+} 
+sideButtons()
